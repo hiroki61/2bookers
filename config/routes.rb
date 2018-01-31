411de418a672016bbe45549d84b447749ea3f030
北川弘貴 <hiroki.k@kitagawahiroshitakanoMacBook-puro.local>
2018-01-31 17:13:06 +0900

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  resources :users, only: [:index, :show, :edit, :update]
- resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
+ resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
 
  # resources :books
  # get '/top' => 'books#top'ではなく以下のように記述することでURLがhttp://localhost:3000/topではなくhttp://localhost:3000/でよくなる。
